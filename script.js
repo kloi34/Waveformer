20d4741a01309ccbb1830c8d8cd4f998f19254f0
kloi34 <53842237+kloi34@users.noreply.github.com>
2022-05-21 14:40:29 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function visualizeAudio(url) {
 
 function filterData(audioBuffer) {
   const rawData = audioBuffer.getChannelData(0);
-  const samples = (audioBuffer.length / audioBuffer.sampleRate) * 1000;
+  const samples = Math.floor(1000 * audioBuffer.duration)
   const blockSize = Math.floor(rawData.length / samples);
   const filteredData = [];
   for (let i = 0; i < samples; i++) {
@@ -22,23 +22,27 @@ function filterData(audioBuffer) {
 }
 
 function visualize(audioBuffer) {
-  const displayedData = 10000
-  const canvasHeight = 200;
-  const canvasWidth = (audioBuffer.length / audioBuffer.sampleRate) * 1000;
+  console.log(audioBuffer.duration*1000);
+  // const displayedData = 1000;
+  // const canvasHeight = 200;
+  // const canvasWidth = (audioBuffer.length / audioBuffer.sampleRate) * 1000;
   const filteredData = filterData(audioBuffer);
+  console.log(filteredData.length)
+  //const displayedData = (audioBuffer.length / audioBuffer.sampleRate) * 1000;
+  const displayedData = filteredData.length;
   const body = document.querySelector('body');
-  const canvas = document.createElement('canvas');
-  canvas.width = canvasWidth;
-  canvas.height = canvasHeight;
-  canvas.style.border = "1px solid #000000";
-  const ctx = canvas.getContext("2d");
-  for (let i = 0; i < displayedData; i++) {
-    const dataPoint = filteredData[i];
-    const xCord = canvasWidth * (i / filteredData.length);
-    ctx.moveTo(xCord, 0);
-    ctx.lineTo(xCord, canvasHeight * dataPoint);
-    ctx.stroke();
-  }
+  // const canvas = document.createElement('canvas');
+  // canvas.width = canvasWidth;
+  // canvas.height = canvasHeight;
+  // canvas.style.border = "1px solid #000000";
+  // const ctx = canvas.getContext("2d");
+  // for (let i = 0; i < displayedData; i++) {
+  //   const dataPoint = filteredData[i];
+  //   const xCord = canvasWidth * (i / filteredData.length);
+  //   ctx.moveTo(xCord, 0);
+  //   ctx.lineTo(xCord, canvasHeight * dataPoint);
+  //   ctx.stroke();
+  // }
   // const durationInSeconds = audioBuffer.length / audioBuffer.sampleRate;
   // for (let i = 0; i < durationInSeconds; i++) { 
   //   const xCord = canvasWidth * i / durationInSeconds;
@@ -51,7 +55,10 @@ function visualize(audioBuffer) {
   // }
   const firstNum = 15;
   const secondNum = 15;
-  for (let i = 0; i < displayedData; i++) {
+  let times = [];
+  let dummy = [];
+  let count = 0;
+  for (let i = 0; i < displayedData - firstNum - secondNum; i++) {
     let firstmax = -Infinity;
     let firstSum = 0;
     let nextmin = Infinity;
@@ -70,22 +77,42 @@ function visualize(audioBuffer) {
         nextmin = Math.min(nextmin, filteredData[i + j]);
       }
     }
-    if (firstmax < nextmin && nextSum > firstSum - 0.15 * firstNum ) {
-      const xCord = canvasWidth * (i / filteredData.length) + firstNum;
-      ctx.moveTo(xCord, canvasHeight);
-      ctx.lineTo(xCord, 0);
-      ctx.stroke();    
-      ctx.font = "30px Arial";
-      ctx.fillText((i + firstNum) + "ms", xCord, canvasHeight);
-      console.log(i + firstNum);
+    if ((firstmax < nextmin) && (nextSum > firstSum - 0.15 * firstNum) && (filteredData[i + firstNum] > filteredData[i + firstNum - 1])) {
+      // const xCord = canvasWidth * (i / filteredData.length) + firstNum;
+      // ctx.moveTo(xCord, canvasHeight);
+      // ctx.lineTo(xCord, 0);
+      // ctx.stroke();    
+      // ctx.font = "30px Arial";
+      // ctx.fillText((i + firstNum) + "ms", xCord, canvasHeight);
+      
+      // correcting
+       //227134 -- > 518
+      //29000 -- > 60
+      //let correctingCoeff = Math.floor(491 * i / (displayedData - firstNum - secondNum))
+      let correctingCoeff = Math.floor((0.002275 * filteredData.length) * i / (displayedData - firstNum - secondNum))
+      //227134 -- > 518
+      //29000 -- > 60
+      //216196 -- > 491
+     times.push(i + firstNum - correctingCoeff)
+     //times.push(i + firstNum)
       i += firstNum + secondNum;
     }
+    dummy.push(0)
+    count++
   }
-  body.appendChild(canvas);
+  console.log(dummy.length, count, firstNum)
+  // body.appendChild(canvas);
+
+  for (let i = 0; i < times.length; i++) {
+    let para = document.createElement("pre");
+    para.textContent = `- StartTime: ${times[i]}
+  Bpm: 0.05000000074505806`;
+    body.appendChild(para);
+  }
 }
 
   
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioContext = new AudioContext();
-visualizeAudio("test.mp3") 
-//visualizeAudio("audio.mp3");
+//visualizeAudio("test.mp3") ;
+visualizeAudio("audio2.mp3");
